46d420561b714bd39461f90517feceb8f1496a7c
liuliuwenbo <15630778839@163.com>
2024-06-13 23:53:04 +0800

    feat: class

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ enum result_mune {
     reach = 60,
     excellent = 100
 }
+
 let student_arr: { name: string, sex: string, result: number, [k:string]:any }[] = [
     {name: '王特1', sex: '男', result: 66},
     {name: '张雨晴', sex: '女', result: 26},
@@ -13,6 +14,7 @@ let student_arr: { name: string, sex: string, result: number, [k:string]:any }[]
     {name: '飞哥', sex: '男', result: 100},
     {name: '小光', sex: '男', result: 0}
 ]
+
 // filter map
 const filter_result = (students:{ name: string, sex: string, result: number ,[k:string]:any}[]):void => {``
     student_arr = students.map(i => {
